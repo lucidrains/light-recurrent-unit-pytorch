6b249194142fae6442c75103031e37f04f958224
lucidrains <lucidrains@gmail.com>
2024-08-30 07:25:53 -0700

    go off script

--- light_recurrent_unit_pytorch/__init__.py
+++ light_recurrent_unit_pytorch/__init__.py
@@ -2,5 +2,6 @@ from light_recurrent_unit_pytorch.light_recurrent_unit import (
     LightRecurrentUnitCell,
     LightRecurrentUnitLayer,
     LightRecurrentUnit,
+    GatedLightRecurrentUnit,
     LightRecurrentUnitBlock
 )
